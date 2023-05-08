a44c740dce63d07f421957e48aba8f3db3010e6c
toteload <toteload@users.noreply.github.com>
2023-05-08 22:30:20 +0200

    WIP on getting the bot talking to the outside world

--- src/game.rs
+++ src/game.rs
@@ -121,10 +121,43 @@ pub enum Rank {
     Flag,
 }
 
+pub const STARTING_RANKS: [Rank; 8] = [
+    Rank::Spy, Rank::Scout, Rank::Scout, Rank::Miner, Rank::General, Rank::Marshal, Rank::Bomb, Rank::Flag,
+];
+
 impl Rank {
     pub fn is_moveable(&self) -> bool {
         use Rank::*;
         !matches!(&self, Flag | Bomb)
+    }
+
+    pub fn from_str(s: &str) -> Option<Rank> {
+        let rank = match s {
+            "Spy" => Rank::Spy,
+            "Scout" => Rank::Scout,
+            "Miner" => Rank::Miner,
+            "General" => Rank::General,
+            "Marshal" => Rank::Marshal,
+            "Bomb" => Rank::Bomb,
+            "Flag" => Rank::Flag,
+            _ => return None,
+        };
+
+        Some(rank)
+    }
+
+    pub fn to_str(&self) -> &'static str {
+        use Rank::*;
+        match &self {
+            Spy => "Spy",
+            Scout => "Scout",
+            Miner => "Miner",
+            General => "General",
+            Marshal => "Marshal",
+            Bomb => "Bomb",
+            Flag => "Flag",
+            _ => unreachable!(),
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ mod bot;
 mod game;
 mod game_coordinator;
 
-use bot::{AgressoBot, RandoBot};
-use game::Rank;
+use bot::{Bot, AgressoBot, RandoBot};
+use game::{Position, Rank, STARTING_RANKS};
 use game_coordinator::{GameCoordinator, Outcome};
 use rand::{Rng, RngCore, SeedableRng};
 use rand_xoshiro::SplitMix64;
 use rayon::prelude::*;
 use std::time::Instant;
+use serde::Deserialize;
+use serde_json::{json, Map, Value};
 
 fn reservoir_sample_one<T, I: Iterator<Item = T>, R: Rng>(rng: &mut R, mut source: I) -> Option<T> {
     let mut chosen = source.next()?;
@@ -52,7 +54,115 @@ fn reservoir_sample<T, I: Iterator<Item = T>, R: Rng>(
     samples
 }
 
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+struct PartialGameInitJson {
+    You: usize,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+struct PositionJson {
+    X: u8,
+    Y: u8,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+struct MoveJson {
+    From: PositionJson,
+    To: PositionJson,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+struct AttackerJson {
+    Player: usize,
+    Rank: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+struct BattleResultJson {
+    Attacker: AttackerJson,
+    Defender: AttackerJson,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+struct PartialGameStateJson {
+    ActivePlayer: usize,
+    TurnNumber: usize,
+    LastMove: Option<MoveJson>,
+    BattleResult: Option<BattleResultJson>,
+}
+
+struct Communicator {
+    bot: Box<dyn Bot>,
+}
+
+impl Communicator {
+    fn new(bot: Box<dyn Bot>) -> Communicator {
+        Communicator {
+            bot,
+        }
+    }
+
+    fn run(&mut self) {
+        println!("bot-start");
+
+        let mut line_buffer = String::new();
+        let stdin = std::io::stdin();
+
+        stdin.read_line(&mut line_buffer).expect("");
+
+        let game_init: PartialGameInitJson = serde_json::from_str(&line_buffer).unwrap();
+
+        let player_id = game_init.You;
+
+        let placement = self.bot.get_initial_placement(&STARTING_RANKS);
+
+        fn pos_to_json(pos: &Position) -> Value {
+            json!({ "X": pos.x, "Y": pos.y })
+        }
+
+        let setup_pieces = placement.iter().map(|(rank, pos)| {
+            json!({
+                "Rank": rank.to_str().to_string(),
+                "Position": pos_to_json(pos),
+            })
+        }).collect::<Vec<Value>>(); 
+
+        let setup_board_command = json!({ "Pieces": setup_pieces });
+
+        println!("{}", serde_json::to_string(&setup_board_command).expect(""));
+
+        loop {
+            line_buffer.clear();
+            stdin.read_line(&mut line_buffer).unwrap();
+            let game_state: PartialGameStateJson = serde_json::from_str(&line_buffer).unwrap();
+
+            if let Some(battle_result) = game_state.BattleResult {
+                todo!();
+            }
+
+            if game_state.ActivePlayer == player_id {
+                // Make your move...
+            } else {
+                // Inform bot of opponents move.
+            }
+        }
+    }
+}
+
 fn main() {
+    let mut seeder = SplitMix64::seed_from_u64(5498709864);
+    let bot = Box::new(AgressoBot::new(seeder.next_u64()));
+    let mut communicator = Communicator::new(bot);
+    communicator.run();
+}
+
+fn main2() {
     let starting_ranks = vec![
         Rank::Marshal,
         Rank::General,
